4a9a06d942217a78aa40440cb31698972d46ce6d
Markus Weber <weber@filics.eu>
2021-05-12 12:23:08 +0200

    fix: freezing bug
    * Change to USB port to prevent freezing bug
    * Mostly did appear on high frequency communication

--- rosserial_mbed/src/ros_lib/MbedHardware.h
+++ rosserial_mbed/src/ros_lib/MbedHardware.h
@@ -9,30 +9,33 @@
 #define ROS_MBED_HARDWARE_H_
 
 #include "mbed.h"
+#include "USBSerial.h"
 
 class MbedHardware {
   public:
     MbedHardware(PinName tx, PinName rx, int baud = 115200)
       : iostream(tx, rx, baud) {
-      baud_ = baud;
-      t.start();
+    baud_ = baud;
+    t.start();
+    iostream.set_blocking(true);
     }
 
     MbedHardware()
-      : iostream(USBTX, USBRX, 115200) {
-        baud_ = 115200;
-        t.start();
+      : iostream(USBTX, USBRX) {
+    baud_ = 115200;
+    t.start();
+    iostream.set_blocking(true);
     }
 
     void setBaud(int baud){
       this->baud_= baud;
-      iostream.set_baud(baud_);
+      // iostream.set_baud(baud_);
     }
 
     int getBaud(){return baud_;}
 
     void init(){
-        iostream.set_baud(baud_);
+        // iostream.set_baud(baud_);
     }
 
     int read(){
@@ -53,7 +56,7 @@ class MbedHardware {
     unsigned long time(){return chrono::duration_cast<chrono::milliseconds>(t.elapsed_time()).count();}
 
 protected:
-    BufferedSerial iostream;
+    USBSerial iostream;
     int baud_;
     Timer t;
 };
